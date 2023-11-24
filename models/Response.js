e99f4f974ffc36a95292fa028ebb09b41fb9474d
rasmi <rasmiranjanmohanty67@gmail.com>
2023-11-25 00:02:50 +0530

    push

---
+++ models/Response.js
@@ -0,0 +1,11 @@
+
+class Response {
+    //boolean,string,obj
+    constructor(error, message, data) {
+        this.error = error;
+        this.message = message;
+        this.response = data;
+    }
+}
+
+module.exports = Response;
